dbf1d9473ba0f67d12a7c703ff3e1b0fed789e16
Анастасия Крюкова <Nastasiya2317@yandex.ru>
2023-08-22 16:19:43 +0300

    test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
             ],
         },
     },
-]
+] 
 
 WSGI_APPLICATION = "api_yamdb.wsgi.application"
 
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
+
 User = get_user_model()
 
 
