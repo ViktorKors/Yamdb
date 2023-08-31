13dd71c6ce41d05dedfaa3dea80bb66c28ab6fe0
Виктор <vitya1313vitya@gmail.com>
2023-08-31 17:24:11 +0300

    Изменил немного пермишн для админов.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,38 +19,35 @@ class AdminOrModeratorOrReadOnly(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.user.is_authenticated and request.user.is_superuser
+        return request.user.is_authenticated and (
+                request.user.is_admin or request.user.is_superuser
         )
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.method in permissions.SAFE_METHODS
-            or (
+        return bool(request.method in permissions.SAFE_METHODS or (
                 request.user.is_authenticated
                 and (request.user.is_admin or request.user.is_superuser)
-            )
-        )
+        ))
 
 
 class AuthorOrAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         return bool(
             request.method in permissions.SAFE_METHODS
             or (
-                request.user.is_authenticated
-                and (
-                    obj.author == request.user
-                    or request.user.is_moderator
-                    or request.user.is_admin
-                )
+                    request.user.is_authenticated
+                    and (
+                            obj.author == request.user
+                            or request.user.is_moderator
+                            or request.user.is_admin
+                    )
             )
         )
